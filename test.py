30b267e4413527c110acd36c4df8155664a2abfa
Pravin Kedar <kedi1992@gmail.com>
2022-06-30 08:24:20 +0530

    test file changes

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("Hello")
+from datetime import datetime, timedelta
+
+data_string = f"\n{datetime.today().strftime('%Y-%m-%d: %H:%M:%S')} log dumped"
+
+with open('data.txt', 'a') as fp:
+    fp.write(data_string)
+    fp.close()
